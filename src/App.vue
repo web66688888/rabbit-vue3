d982ee07b324fedba6ec688aa47ab9a384557276
webyf <654897576@qq.com>
2022-07-05 14:23:54 +0800

    feat:init start

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-    App组件
+   <h1>1111</h1>
 </template>
   
 <script setup lang='ts'>
-  
+  import  axios  from "./utils/request";
+   axios.get('/home/index').then(res=>{
+    console.log(res);
+    
+   })
 </script>
   
-<style>
+<style scoped lang="less">
+@import '@/assets/styles/variables.less';
+@import '@/assets/styles/mixins.less';
+ h1 {
+   color: @xtxColor;
+   background-color:pink;
+   .hoverShadow ()
+ }
+
   
 </style>
